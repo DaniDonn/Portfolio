2dc0dc8e2e99dc00bb96b1811be95870611625f0
Michael Santos <mjs8837@rit.edu>
2025-05-18 00:16:23 -0400

    Website progress.
    Fixed spaces in HTML page names.
    Added next and previous buttons to each page.
    Added next and previous button functionality to index.js.

--- js/index.js
+++ js/index.js
@@ -1,32 +1,102 @@
-const toggle = document.querySelectorAll(".infoToggle")
+const toggle = document.querySelectorAll(".infoToggle");
 
-console.log('JavaScript is loaded!');
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
+const nextProjectButton = document.querySelector("#nextProject");
+const previousProjectButton = document.querySelector("#previousProject");
+const htmlLinkObjects = document.querySelectorAll('.hiddenLink');
 
-navLinks.forEach(link => {
-    link.addEventListener ('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-});
+let currentPageString = '';
+const htmlLinks = [];
 
-toggle.forEach(toggleObj => {
+document.addEventListener("DOMContentLoaded", (event) => {
+    main();
+})
 
-    // toggle the appropriate text when a button is clicked
-    toggleObj.addEventListener("click", () => {
-        
-        // Getting the next element (the UL in this case)
-        nextElement = toggleObj.nextElementSibling;
+function main(){
+    let currentPageHTML = '';
+    let currentPageIndex = 0;
 
-        // Check if the content is hidden or not and toggle visibility
-        if (nextElement.style.display === "none" || nextElement.style.display === "") {
-            nextElement.style.display = "block"; // Show content
-        } else {
-            nextElement.style.display = "none"; // Hide content
+    // Splitting the url to only get the non HTML info.
+    const urlSplit = window.location.href.split('/');
+
+    // Looping through all parts of the split url string.
+    urlSplit.forEach(link => {
+
+        // Adding to the link string unless the split contains 'html'.
+        if (!link.includes('html')){
+            currentPageString += link + '/';
+        }
+        // Storting the html link.
+        else {
+            currentPageHTML = link;
+        }
+    });
+
+    // Adding the HTML linkes for each page.
+    htmlLinkObjects.forEach(link => {
+        htmlLinks.push(link.getAttribute('href'));
+    });
+
+    // Checking through each link to see what page we're on.
+    htmlLinks.forEach(link => {
+        // Comparing the current page HTML string to all the stored HTML link strings to determine what page this is.
+        if (currentPageHTML.toString() === link.toString()) {
+            // Set the current page index to know which page is next and which is previous.
+            currentPageIndex = htmlLinks.indexOf(link);
         }
     })
-});
+
+    // Adding a click event to the next page button. 
+    nextProjectButton.addEventListener('click', () => {
+        ChangePage(currentPageIndex + 1)
+    })
+
+    // Adding a click event to the previous page button. 
+    previousProjectButton.addEventListener('click', () => {
+        ChangePage(currentPageIndex - 1)
+    })
+    
+    function HandleInfoToggle(toggleObject) {
+        // Loop through each toggle object in the toggle array.
+        toggle.forEach(toggleObj => {
+            // If it's not the one that was clicked, hide it.
+            if (toggleObject != toggleObj) {
+                toggleObj.nextElementSibling.style.display = "none";
+            }
+        })
+    }
+    
+    toggle.forEach(toggleObj => {
+    
+        // toggle the appropriate text when a button is clicked
+        toggleObj.addEventListener("click", () => {
+    
+            HandleInfoToggle(toggleObj);
+            
+            // Getting the next element (the UL in this case)
+            const nextElement = toggleObj.nextElementSibling;
+    
+            // Check if the content is hidden or not and toggle visibility
+            if (nextElement.style.display === "none" || nextElement.style.display === "") {
+                nextElement.style.display = "block"; // Show content
+            } else {
+                nextElement.style.display = "none"; // Hide content
+            }
+        })
+    });
+}
+
+function ChangePage(newIndex) {
+    // Preventing the index from being out of bounds.
+    if (newIndex > htmlLinks.length - 1) {
+        newIndex = 0;
+    }
+    else if (newIndex < 0) {
+        newIndex = htmlLinks.length - 1;
+    }
+
+    // Setting the current link address to the new page.
+    window.location.href = currentPageString + htmlLinks[newIndex];
+}
